c824e9f3d0d3ebe32f6b22cb9921544cb43716c5
cyd1992 <315309286@qq.com>
2015-10-25 10:51:44 +0800

    init two test
    audio test and dj test

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,7 @@
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
+#include "MenuScene.h"
+
 
 USING_NS_CC;
 
@@ -7,6 +9,8 @@ static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+//add
+static cocos2d::Size currentResolutionSize = cocos2d::Size(1920, 1080);
 
 AppDelegate::AppDelegate() {
 
@@ -54,8 +58,16 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0 / 60);
 
     // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+
+	//for windows debug////////////////////
+	glview->setFrameSize(1920/2, 1080/2);
+	///////////////////////////////////////
+
+
+    glview->setDesignResolutionSize(currentResolutionSize.width/2, currentResolutionSize.height/2, ResolutionPolicy::NO_BORDER);
     Size frameSize = glview->getFrameSize();
+
+	/*
     // if the frame's height is larger than the height of medium size.
     if (frameSize.height > mediumResolutionSize.height)
     {        
@@ -71,11 +83,11 @@ bool AppDelegate::applicationDidFinishLaunching() {
     {        
         director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
     }
-
+	*/
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    auto scene = MenuScene::createScene();
 
     // run
     director->runWithScene(scene);
