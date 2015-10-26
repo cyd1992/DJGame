4416b8acce9894a599722404c09806f8c9cb5cd6
cyd1992 <315309286@qq.com>
2015-10-26 13:37:13 +0800

    add disk img
    add disk.png

--- Classes/DiskTestScene.cpp
+++ Classes/DiskTestScene.cpp
@@ -48,6 +48,15 @@ bool DiskTestScene::init()
 
 	////////////////////////////
 
+	// add "HelloWorld" splash screen"
+	auto sprite = Sprite::create("disk.png");
+
+	// position the sprite on the center of the screen
+	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+
+	// add the sprite as a child to this layer
+	this->addChild(sprite, 0);
+
 	return true;
 }
 
